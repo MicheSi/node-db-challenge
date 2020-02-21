a58b6e24f65175d9a9661e8994aaa2569101542c
mlazzell17@gmail.com <MicheSi>
2020-02-21 10:42:15 -0700

    get and post working for tasks - mvp completed

--- projects/project-model.js
+++ projects/project-model.js
@@ -2,7 +2,9 @@ const db = require('../data/db-config');
 
 module.exports = {
     find,
-    add
+    add,
+    findTasks,
+    addTask
 }
 
 function find() {
@@ -12,4 +14,17 @@ function find() {
 function add(project) {
     return db('projects')
         .insert(project, 'id')
+}
+
+function findTasks(id) {
+    return db('tasks as t')
+        .join('projects as p', 'p.id', 't.project_id')
+        .select('t.description', 't.notes', 't.completed', 'p.name as project_name', 'p.description as project_description')
+        .where('project_id', id)
+}
+
+function addTask(task, project_id) {
+    const newTask = {...task, project_id}
+    return db('tasks')
+        .insert(newTask)
 }
--- projects/project-router.js
+++ projects/project-router.js
@@ -14,12 +14,37 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id/tasks', (req, res) => {
+    const id = req.params.id;
+
+    Projects.findTasks(id)
+    .then(tasks => {
+        res.status(200).json(tasks)
+    })
+    .catch(err => {
+        res.status(500).json({message: err.message})
+    })
+})
+
 router.post('/', (req, res) => {
     const projectInfo = req.body;
 
     Projects.add(projectInfo)
     .then(ids => {
         res.status(200).json({created: ids[0]})
+    })
+    .catch(err => {
+        res.status(500).json({message: err.message})
+    })
+})
+
+router.post('/:id/tasks', (req, res) => {
+    const taskInfo = req.body;
+    const id = req.params.id;
+
+    Projects.addTask(taskInfo, id)
+    .then(task => {
+        res.status(200).json(task)
     })
     .catch(err => {
         res.status(500).json({message: err.message})
